510a20f108b5e35ae6d864e407936f692bf7e903
Rishikesh Kale <>
2022-08-03 21:28:25 +0530

    Deploying Website

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,73 @@
-const {src, task}= require('gulp');
-const ghPages = require('gulp-gh-pages');
+const gulp = require("gulp");
+const gap = require("gulp-append-prepend");
 
-task('deploy', () => src('./dist/**/*').pipe(ghPages()));
+gulp.task("licenses", async function () {
+  // this is to add Rishikesh Kale licenses in the production mode for the minified js
+  gulp
+    .src("build/static/js/*chunk.js", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* Notus React - v1.1.0
+=========================================================
+
+* Copyright 2022 BrandVilla
+
+* Coded by Rishikesh Kale
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Rishikesh Kale licenses in the production mode for the minified html
+  gulp
+    .src("build/index.html", { base: "./" })
+    .pipe(
+      gap.prependText(`<!--
+
+=========================================================
+* Notus React - v1.1.0
+=========================================================
+
+* Copyright 2021 Rishikesh Kale 
+* Licensed under MIT
+
+* Coded by Rishikesh Kale
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+-->`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+
+  // this is to add Rishikesh Kale licenses in the production mode for the minified css
+  gulp
+    .src("build/static/css/*chunk.css", { base: "./" })
+    .pipe(
+      gap.prependText(`/*!
+
+=========================================================
+* Notus React - v1.1.0
+=========================================================
+
+* Copyright 2021 Rishikesh Kale (https://www.creative-tim.com)
+* Licensed under MIT 
+
+* Coded by Rishikesh Kale
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/`)
+    )
+    .pipe(gulp.dest("./", { overwrite: true }));
+  return;
+});
